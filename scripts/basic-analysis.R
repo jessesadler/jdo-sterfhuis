1b313bc1c374a0cf4ca50ada49b3931cd647b95a
Jesse Sadler <jsadler@ucla.edu>
2017-08-31 13:32:12 -0700

    script for sum with gr, sc, d

--- scripts/basic-analysis.R
+++ scripts/basic-analysis.R
@@ -12,17 +12,42 @@ transactions <- transactions %>% select(from, to, gr:pounds)
 
 ### Decimal vlams ###
 
-transactions <- mutate(transactions, vlams_dec = gr + sc / 20 + d / 240)
+transactions_dec <- mutate(transactions, vlams_dec = gr + sc / 20 + d / 240)
 
+credit_dec <- transactions_dec %>% group_by(from) %>% summarise(credit = sum(vlams_dec))
+debit_dec <- transactions_dec %>% group_by(to) %>% summarise(debit = sum(vlams_dec))
 
-credit <- transactions %>% group_by(from) %>% summarise(credit = sum(vlams_dec))
-debit <- transactions %>% group_by(to) %>% summarise(debit = sum(vlams_dec))
-
-accounts_sum <- full_join(credit, debit, by = c("from" = "to")) %>% 
+accounts_sum <- full_join(credit_dec, debit_dec, by = c("from" = "to")) %>% 
   replace_na(list(credit = 0, debit = 0)) %>% 
   rename(id = from)
 
-accounts_sum <- accounts_sum %>% mutate(current = credit - debit) %>% 
-  arrange(current)
+current <- accounts_sum %>% mutate(current = credit - debit)
 
-open <- filter(accounts_sum, current >= 1/240 | current <= -1/240)
+open <- filter(current, current >= 0.001 | current <= -0.001) %>% arrange(id)
+closed <- filter(current, near(current, 0))
+
+### Vlams ###
+
+credit_vlams <- transactions %>% group_by(from) %>% summarise(gr = sum(gr), sc = sum(sc), d = sum(d))
+credit_vlams <- mutate(credit_vlams,
+                gr_c = gr + ((sc + d %/% 12) %/% 20),
+                sc_c = (sc + d %/% 12) %% 20,
+                d_c = d %% 12) %>% 
+  select(-(gr:d))
+
+debit_vlams <- transactions %>% group_by(to) %>% summarise(gr = sum(gr), sc = sum(sc), d = sum(d))
+debit_vlams <- mutate(debit_vlams,
+                       gr_d = gr + ((sc + d %/% 12) %/% 20),
+                       sc_d = (sc + d %/% 12) %% 20,
+                       d_d = d %% 12) %>% 
+  select(-(gr:d))
+
+accounts_sum <- full_join(credit_vlams, debit_vlams, by = c("from" = "to")) %>% 
+  replace_na(list(gr_c = 0, sc_c = 0, d_c = 0, gr_d = 0, sc_d = 0, d_d = 0)) %>% 
+  rename(id = from)
+
+current <- mutate(accounts_sum, gr = gr_c - gr_d, sc = sc_c - sc_d, d = d_c - d_d)
+
+open <- select(current, id, gr:d) %>% filter(gr + sc + d != 0)
+
+# Still need to find way to deal with negative numbers in sc and d
--- scripts/basic-graphs.R
+++ scripts/basic-graphs.R
@@ -18,7 +18,13 @@ sterfhuis <- graph_from_data_frame(d = transactions, vertices = accounts, direct
 
 set_graph_style()
 
-ggraph(sterfhuis) + geom_edge_link(aes(alpha = count)) + geom_node_point(aes(color = ac_type), alpha = 0.7)
+ggraph(sterfhuis) + geom_edge_link(aes(alpha = count)) + geom_node_point(alpha = 0.7) + theme(legend.position = "none")
+
+ggraph(sterfhuis, layout = "kk") + 
+  geom_edge_link(aes(alpha = count)) + 
+  geom_node_point(alpha = 0.7) + 
+  theme(legend.position = "none") + 
+  labs(title = "Accounts of the estate of a 16th-century merchant")
 
 ggraph(sterfhuis, layout = "linear") + geom_edge_arc(aes(alpha = count)) + geom_node_point(aes(color = ac_type), alpha = 0.7)
 
